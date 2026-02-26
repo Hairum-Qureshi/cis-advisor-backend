b29b1c02a78afd17f202303f07204a580f7bccd4
Hairum Qureshi <hqureshi@udel.edu>
2026-02-26 06:44:40 -0500

    removed comment

--- api/routes/api.ts
+++ api/routes/api.ts
@@ -11,7 +11,6 @@ router.get("/data-source-json", getDataSourceJSON);
 
 router.delete("/clear-data-source", clearDataSource);
 
-// emphasize that it takes in an array object which is efficient for bulk data
 router.post("/add-data-source", addToDataSource);
 
 router.post("/ask-gemini", queryGemini);
