6fe3ab23062aa27b6d71d508c2fe0ad192a89411
Hairum Qureshi <hqureshi@udel.edu>
2026-03-11 10:11:33 -0400

    appended custom verifyCredentials middleware before admin routes

--- api/routes/api.ts
+++ api/routes/api.ts
@@ -7,18 +7,19 @@ import {
 	queryGemini,
 	regenerateEmbeddings
 } from "../controllers/api";
+import verifyCredentials from "../middleware/verifyCredentials";
 const router = express.Router();
 
 router.get("/data-source-json", getDataSourceJSON);
 
-router.delete("/clear-data-source", clearDataSource);
+router.delete("/clear-data-source", verifyCredentials, clearDataSource);
 
-router.post("/add-data-source", addToDataSource);
+router.post("/add-data-source", verifyCredentials, addToDataSource);
 
-router.delete("/q-and-a/:id", deleteQAndAPair);
+router.delete("/q-and-a/:id", verifyCredentials, deleteQAndAPair);
 
 router.post("/ask-gemini", queryGemini);
 
-router.put("/regenerate-embeddings", regenerateEmbeddings);
+router.put("/regenerate-embeddings", verifyCredentials, regenerateEmbeddings);
 
 export default router;
