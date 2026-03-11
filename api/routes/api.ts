1da380636e8534e8fdba7e08f358bf5a9aa78c14
Hairum Qureshi <hqureshi@udel.edu>
2026-03-11 02:08:08 -0400

    added new delete q&a pair by ID endpoint

--- api/routes/api.ts
+++ api/routes/api.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
 	addToDataSource,
 	clearDataSource,
+	deleteQAndAPair,
 	getDataSourceJSON,
 	queryGemini
 } from "../controllers/api";
@@ -12,6 +13,8 @@ router.get("/data-source-json", getDataSourceJSON);
 router.delete("/clear-data-source", clearDataSource);
 
 router.post("/add-data-source", addToDataSource);
+
+router.delete("/q-and-a/:id", deleteQAndAPair);
 
 router.post("/ask-gemini", queryGemini);
 
