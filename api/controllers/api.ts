9d6384749ddbfaa4dac3f6a76e5c80587bc6ec79
Hairum Qureshi <hqureshi@udel.edu>
2026-03-11 10:14:21 -0400

    added comment to 'regenerateEmbeddings' controller function

--- api/controllers/api.ts
+++ api/controllers/api.ts
@@ -184,6 +184,8 @@ const deleteQAndAPair = async (req: Request, res: Response) => {
 };
 
 const regenerateEmbeddings = async (req: Request, res: Response) => {
+	// If you feel that the relevance of the Gemini model's responses has decreased after adding new Q&A pairs to your dataset, you can invoke this endpoint to regenerate all embeddings for the entire dataset. This will ensure that the similarity search is using the most up-to-date embeddings based on the current dataset, which can help improve the relevance of responses when users ask questions related to the newly added data. Keep in mind that regenerating embeddings for a large dataset may take some time, so it's best to use this endpoint during off-peak hours or when you don't expect a high volume of user queries.
+	
 	await dbConnect();
 
 	try {
